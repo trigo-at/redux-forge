dffa0e978c4c10381a79306acec0fbd91b734891
David Wippel <david@trigo.at>
2018-06-06 11:15:34 +0200

    remove ramda

--- default-reducer.js
+++ default-reducer.js
@@ -1,35 +1,35 @@
-import {identity} from 'ramda';
+const identity = x => x;
 
 export const requestReducer = {
-    next(state, action) {
-        return Object.assign({}, state, {
-            isLoading: action.payload || true,
-            hasLoaded: false,
-            error: false,
-            response: undefined,
-        });
-    },
+	next(state, action) {
+		return Object.assign({}, state, {
+			isLoading: action.payload || true,
+			hasLoaded: false,
+			error: false,
+			response: undefined,
+		});
+	},
 };
 
 export const transformResponseReducer = (transform = identity) => {
-    return {
-        next(state, action) {
-            return Object.assign({}, state, {
-                response: transform(action.payload),
-                isLoading: false,
-                hasLoaded: true,
-                error: false,
-            });
-        },
-        throw(state, action) {
-            return Object.assign({}, state, {
-                isLoading: false,
-                hasLoaded: false,
-                error: true,
-                response: action.payload,
-            });
-        },
-    };
+	return {
+		next(state, action) {
+			return Object.assign({}, state, {
+				response: transform(action.payload),
+				isLoading: false,
+				hasLoaded: true,
+				error: false,
+			});
+		},
+		throw(state, action) {
+			return Object.assign({}, state, {
+				isLoading: false,
+				hasLoaded: false,
+				error: true,
+				response: action.payload,
+			});
+		},
+	};
 };
 
 export const responseReducer = transformResponseReducer();
--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,6 @@
+import pkg from './package.json';
+
 export default {
-    input: 'forge.js',
-    output: {
-        file: 'forge.cjs.js',
-        format: 'cjs',
-    },
-    external: ['ramda'],
+	input: 'forge.js',
+	output: [{file: pkg.main, format: 'cjs'}, {file: pkg.module, format: 'es'}],
 };
